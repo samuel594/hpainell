2b68929911e77ca39d381ee393ca5f8ed4112b47
marcosboni <74115335+marcosboni@users.noreply.github.com>
2022-02-10 18:42:04 -0300

    Add files via upload

---
+++ avisos.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+   <link rel="stylesheet" href="avisos.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/css/materialize.min.css">
+</head>
+<body>
+    <div class="container">
+       <table>
+            <thead>
+                <tr>
+                    <th>Código</th>
+                    <th>Descrição</th>
+                    <th>Categoria</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>001</td>
+                    <td>Notebook i7 8GB Branco</td>
+                    <td>Informática</td>
+                </tr>
+                <tr>
+                    <td>002</td>
+                    <td>Caneta Esferográfica Azul</td>
+                    <td>Papelaria</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</body>
+</html>
